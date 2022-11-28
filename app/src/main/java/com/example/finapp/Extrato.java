2d2970b4731c6842af478d7c0b551fb9a21c5bb8
janaercole <janaercole@gmail.com>
2022-11-27 22:41:03 -0300

    tela inicial 2

---
+++ app/src/main/java/com/example/finapp/Extrato.java
@@ -0,0 +1,14 @@
+package com.example.finapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Extrato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extrato);
+    }
+}
---
+++ app/src/main/java/com/example/finapp/ListaClassificada.java
@@ -0,0 +1,14 @@
+package com.example.finapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaClassificada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_classificada);
+    }
+}
