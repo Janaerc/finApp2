09ed5dbeaf4e29398f01e14c167aabf314397d6a
janaercole <janaercole@gmail.com>
2022-11-27 22:40:34 -0300

    tela inicial e direcionamento

--- app/src/main/java/com/example/finapp/MainActivity.java
+++ app/src/main/java/com/example/finapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.finapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickExtrato(View view){
+        Intent it = new Intent(this, Extrato.class);
+        startActivity(it);
+
+    }
+
+    public void onClickOperacoes(View view){
+        Intent it = new Intent(this, CadastroOperacoes.class);
+        startActivity(it);
+
+    }
+    public void onClickLista(View view){
+        Intent it = new Intent(this, ListaClassificada.class);
+        startActivity(it);
+
+    }
+    public void onClickPesquisar(View view){
+        Intent it = new Intent(this, Pesquisar.class);
+        startActivity(it);
+
+    }
+
+
+
+
+
 }
